52fda7829e0bb38a14fd9c93e1b0670424eb4c71
Oliviya11 <nastyawnastya@gmail.com>
2019-01-20 17:31:28 +0200

    Find aggregate roots

--- CodeAnalysisAppDDD/Program.cs
+++ CodeAnalysisAppDDD/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
 using System;
 using System.Linq;
@@ -9,6 +11,7 @@ namespace CodeAnalysisAppDDD
     class Program
     {
         const string TEST_SOLUTION = @"D:\ProjectsOAAD\ConsoleApp1\ConsoleApp1.sln";
+        const string AGGREGATE_INTERFACE = "IAggregateRoot";
         
         static void Main(string[] args)
         {
@@ -19,6 +22,31 @@ namespace CodeAnalysisAppDDD
                 Project proj = soln.Projects.Single();
                 Console.WriteLine("Start analyzing project: " + proj.Name + ", " + proj.Language);
                 Compilation compilation = proj.GetCompilationAsync().Result;
+
+                foreach (var tree in compilation.SyntaxTrees)
+                {
+                    var classes = tree.GetRoot().DescendantNodesAndSelf().Where(x => x.IsKind(SyntaxKind.ClassDeclaration));
+                    if (classes != null)
+                    {
+                        foreach (var c in classes)
+                        {
+                            var classDec = (ClassDeclarationSyntax)c;
+                            var bases = classDec.BaseList;
+                            if (bases != null)
+                            {
+                                foreach (var b in bases.Types)
+                                {
+                                    var nodeType = compilation.GetSemanticModel(tree).GetTypeInfo(b.Type);
+                                    string name = (nodeType.Type == null) ? "" : nodeType.Type.Name;
+                                    if (AGGREGATE_INTERFACE.Equals(name))
+                                    {
+                                        Console.WriteLine(classDec.Identifier.Text);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
             catch (ReflectionTypeLoadException ex)
             {
