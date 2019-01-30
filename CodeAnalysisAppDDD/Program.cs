d48ca0d3529c4a2c3dc12f33761ea28b65259cdb
Oliviya11 <nastyawnastya@gmail.com>
2019-01-30 21:58:35 +0200

    Code refactoring

--- CodeAnalysisAppDDD/Program.cs
+++ CodeAnalysisAppDDD/Program.cs
@@ -29,7 +29,8 @@ namespace CodeAnalysisAppDDD
                 // Collect IAggregateRoot roots
                 foreach (var tree in compilation.SyntaxTrees)
                 {
-                    var classes = tree.GetRoot().DescendantNodesAndSelf().Where(x => x.IsKind(SyntaxKind.ClassDeclaration));
+                    var classes = tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
+                    //tree.GetRoot().DescendantNodesAndSelf().Where(x => x.IsKind(SyntaxKind.ClassDeclaration));
                     if (classes != null)
                     {
                         foreach (var c in classes)
@@ -48,47 +49,35 @@ namespace CodeAnalysisAppDDD
                                     }
                                 }
                             }
-                        }
-                    }
-                }
 
-                // Collect references
-                Console.WriteLine("Collect references...");
-                foreach (var tree in compilation.SyntaxTrees)
-                {
-                    SemanticModel model = compilation.GetSemanticModel(tree);
-                    //  var classes = tree.GetRoot().DescendantNodesAndSelf().Where(x => x.IsKind(SyntaxKind.ClassKeyword));
-                    var classes = tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
-                    foreach (var c in classes)
-                    {
-                        var classSymbol = model.GetDeclaredSymbol(c);
-                        var referencesToClass = SymbolFinder.FindReferencesAsync(classSymbol, proj.Solution).Result;
-                        // get span from locations:
-                        foreach (var referenceToClass in referencesToClass)
-                        {
-                            var locations = referenceToClass.Locations;
-                            foreach (var loc in locations)
+                            // Collect references
+                            SemanticModel model = compilation.GetSemanticModel(tree);
+                            var classSymbol = model.GetDeclaredSymbol(c);
+                            var referencesToClass = SymbolFinder.FindReferencesAsync(classSymbol, proj.Solution).Result;
+                            // get span from locations:
+                            foreach (var referenceToClass in referencesToClass)
                             {
-                                var location = loc.Location;
-                                var span = location.SourceSpan;
-                                // find node:
-                                var sourceTree = location.SourceTree;
-                                var sourceNodes = sourceTree.GetRoot().DescendantNodes().Where(x => x.Span.IntersectsWith(span));
-                                foreach (var node in sourceNodes)
+                                var locations = referenceToClass.Locations;
+                                foreach (var loc in locations)
                                 {
-                                    var classDeclaration = node as ClassDeclarationSyntax;
-                                    if (classDeclaration != null)
+                                    var location = loc.Location;
+                                    var span = location.SourceSpan;
+                                    // find node:
+                                    var sourceTree = location.SourceTree;
+                                    var sourceNodes = sourceTree.GetRoot().DescendantNodes().Where(x => x.Span.IntersectsWith(span));
+                                    foreach (var node in sourceNodes)
                                     {
-                                        var referenceTo = c.Identifier.Text;
-                                        var referenceFrom = classDeclaration.Identifier.Text;
-                                        Console.WriteLine(referenceFrom + " -> " + referenceTo);
+                                        var classDeclaration = node as ClassDeclarationSyntax;
+                                        if (classDeclaration != null)
+                                        {
+                                            var referenceTo = c.Identifier.Text;
+                                            var referenceFrom = classDeclaration.Identifier.Text;
+                                            Console.WriteLine(referenceFrom + " -> " + referenceTo);
+                                        }
                                     }
                                 }
                             }
                         }
-                        //var nodes = sf.GetRoot().DescendantNodes().Where(x => x.Span.IntersectsWith(span));
-                        // from node recursevly get Ancestors
-                        // fo each ancestor define if it is ClassDeclarationSyntax type;
                     }
                 }
             }
