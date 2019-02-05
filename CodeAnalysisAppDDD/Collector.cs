e23f41ffd641b3ac63c7257f51ef7ffac65b4156
Oliviya11 <nastyawnastya@gmail.com>
2019-02-06 00:02:34 +0200

    Code refactoring

---
+++ CodeAnalysisAppDDD/Collector.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FindSymbols;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CodeAnalysisAppDDD
+{
+    class Collector
+    {
+        const string TEST_SOLUTION = @"D:\ProjectsOAAD\ConsoleApp1\ConsoleApp1.sln";
+        const string AGGREGATE_INTERFACE = "IAggregateRoot";
+
+        public struct Result
+        {
+            public HashSet<string> aggregateRoots;
+            public Digraph digraph;
+            public Digraph reversedDigraph;
+        }
+
+        Result result = new Result();
+
+        public void startCollecting()
+        {
+            result.aggregateRoots = new HashSet<string>();
+            result.digraph = new Digraph();
+            result.reversedDigraph = new Digraph();
+
+            try
+            {
+                MSBuildWorkspace ws = MSBuildWorkspace.Create();
+                Solution soln = ws.OpenSolutionAsync(TEST_SOLUTION).Result;
+                List<Project> projects = soln.Projects.ToList<Project>();
+                Project proj = projects[1];
+                Console.WriteLine("Start collecting info about project: " + proj.Name + ", " + proj.Language);
+                Console.WriteLine();
+                Compilation compilation = proj.GetCompilationAsync().Result;
+
+                pushVerticesClassesToDigraph(result.digraph, compilation.SyntaxTrees);
+
+                foreach (var tree in compilation.SyntaxTrees)
+                {
+                    var classes = tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
+                    if (classes != null)
+                    {
+                        foreach (var c in classes)
+                        {
+                            // Collect IAggregateRoot roots
+                            var classDec = c as ClassDeclarationSyntax;
+                            if (classDec != null)
+                            {
+                                collectAggregateRoots(classDec, compilation, tree);
+                            }
+
+                            // Collect references
+                            SemanticModel model = compilation.GetSemanticModel(tree);
+                            collectReferences(model, c, c.Identifier.Text, proj.Solution, result.digraph);
+                        }
+                    }
+                }
+
+                result.reversedDigraph = result.digraph.reverse();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                foreach (Exception inner in ex.LoaderExceptions)
+                {
+                    Console.WriteLine(inner.Message);
+                }
+            }
+        }
+
+        void collectAggregateRoots(ClassDeclarationSyntax classDec, Compilation compilation, SyntaxTree tree)
+        {
+            var bases = classDec.BaseList;
+            if (bases != null)
+            {
+                foreach (var b in bases.Types)
+                {
+                    var nodeType = compilation.GetSemanticModel(tree).GetTypeInfo(b.Type);
+                    string name = (nodeType.Type == null) ? "" : nodeType.Type.Name;
+                    if (AGGREGATE_INTERFACE.Equals(name))
+                    {
+                        string className = classDec.Identifier.Text;
+                        result.aggregateRoots.Add(className);
+                    }
+                }
+            }
+        }
+
+        void collectReferences(SemanticModel model, SyntaxNode c, string className, Solution solution, Digraph digraph)
+        {
+            var classSymbol = model.GetDeclaredSymbol(c);
+            var referencesToClass = SymbolFinder.FindReferencesAsync(classSymbol, solution).Result;
+            // get span from locations:
+            foreach (var referenceToClass in referencesToClass)
+            {
+                var locations = referenceToClass.Locations;
+                foreach (var loc in locations)
+                {
+                    var location = loc.Location;
+                    var span = location.SourceSpan;
+                    // find node:
+                    var sourceTree = location.SourceTree;
+                    var sourceNodes = sourceTree.GetRoot().DescendantNodes().Where(x => x.Span.IntersectsWith(span));
+                    foreach (var node in sourceNodes)
+                    {
+                        var classDeclaration = node as ClassDeclarationSyntax;
+                        if (classDeclaration != null)
+                        {
+                            var referenceFrom = classDeclaration.Identifier.Text;
+                            digraph.addEdge(referenceFrom, className);
+                        }
+                    }
+                }
+            }
+        }
+
+        void pushVerticesClassesToDigraph(Digraph digraph, IEnumerable<SyntaxTree> trees)
+        {
+            foreach (var tree in trees)
+            {
+                var classes = tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
+                if (classes != null)
+                {
+                    foreach (var c in classes)
+                    {
+                        digraph.addVertex(c.Identifier.Text);
+                    }
+                }
+            }
+        }
+
+        public Result getResult()
+        {
+            return result;
+        }
+    }
+}
--- CodeAnalysisAppDDD/Program.cs
+++ CodeAnalysisAppDDD/Program.cs
@@ -1,137 +1,32 @@
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.FindSymbols;
-using Microsoft.CodeAnalysis.MSBuild;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+using static CodeAnalysisAppDDD.Collector;
 
 namespace CodeAnalysisAppDDD
 {
     class Program
-    {
-        const string TEST_SOLUTION = @"D:\ProjectsOAAD\ConsoleApp1\ConsoleApp1.sln";
-        const string AGGREGATE_INTERFACE = "IAggregateRoot";
-        
+    {   
         static void Main(string[] args)
         {
-            try
+            Collector collector = new Collector();
+            collector.startCollecting();
+            Result result = collector.getResult();
+
+            Console.WriteLine("Show names of aggregate roots:");
+            foreach (string name in result.aggregateRoots)
             {
-                MSBuildWorkspace ws = MSBuildWorkspace.Create();
-                Solution soln = ws.OpenSolutionAsync(TEST_SOLUTION).Result;
-                List<Project> projects = soln.Projects.ToList<Project>();
-                Project proj = projects[1];
-                Console.WriteLine("Start analyzing project: " + proj.Name + ", " + proj.Language);
-                Console.WriteLine();
-                Compilation compilation = proj.GetCompilationAsync().Result;
-
-                Digraph digraph = new Digraph();
-                pushVerticesClassesToDigraph(digraph, compilation.SyntaxTrees);
-
-                Console.WriteLine("Collect aggregate roots: ");
-                foreach (var tree in compilation.SyntaxTrees)
-                {
-                    var classes = tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
-                    if (classes != null)
-                    {
-                        foreach (var c in classes)
-                        {
-                            // Collect IAggregateRoot roots
-                            var classDec = c as ClassDeclarationSyntax;
-                            if (classDec != null)
-                            {
-                                collectAggregateRoots(classDec, compilation, tree);
-                            }
-
-                            // Collect references
-                            SemanticModel model = compilation.GetSemanticModel(tree);
-                            collectReferences(model, c, c.Identifier.Text, proj.Solution, digraph);
-                        }
-                    }
-                }
-
-                Console.WriteLine();
-                Console.WriteLine("Show relations between classes (digraph): ");
-                digraph.show();
-
-                Console.WriteLine();
-                Console.WriteLine("Show reversed digraph: ");
-                Digraph reversedDigraph = digraph.reverse();
-                reversedDigraph.show();
+                Console.WriteLine(name + ";");
             }
-            catch (ReflectionTypeLoadException ex)
-            {
-                foreach (Exception inner in ex.LoaderExceptions)
-                {
-                    Console.WriteLine(inner.Message);
-                }
-            }
-            
+            Console.WriteLine();
+
+            Console.WriteLine("Relations between classes (digraph):");
+            result.digraph.show();
+            Console.WriteLine();
+
+            Console.WriteLine("Reversed digraph:");
+            result.reversedDigraph.show();
+            Console.WriteLine();
+
             Console.ReadLine();
-        }
-
-        static void collectAggregateRoots(ClassDeclarationSyntax classDec, Compilation compilation, SyntaxTree tree)
-        {
-            var bases = classDec.BaseList;
-            if (bases != null)
-            {
-                foreach (var b in bases.Types)
-                {
-                    var nodeType = compilation.GetSemanticModel(tree).GetTypeInfo(b.Type);
-                    string name = (nodeType.Type == null) ? "" : nodeType.Type.Name;
-                    if (AGGREGATE_INTERFACE.Equals(name))
-                    {
-                        Console.WriteLine(classDec.Identifier.Text);
-                    }
-                }
-            }
-        }
-
-        static void collectReferences(SemanticModel model, SyntaxNode c, string className, Solution solution, Digraph digraph)
-        {
-            var classSymbol = model.GetDeclaredSymbol(c);
-            var referencesToClass = SymbolFinder.FindReferencesAsync(classSymbol, solution).Result;
-            // get span from locations:
-            foreach (var referenceToClass in referencesToClass)
-            {
-                var locations = referenceToClass.Locations;
-                foreach (var loc in locations)
-                {
-                    var location = loc.Location;
-                    var span = location.SourceSpan;
-                    // find node:
-                    var sourceTree = location.SourceTree;
-                    var sourceNodes = sourceTree.GetRoot().DescendantNodes().Where(x => x.Span.IntersectsWith(span));
-                    foreach (var node in sourceNodes)
-                    {
-                        var classDeclaration = node as ClassDeclarationSyntax;
-                        if (classDeclaration != null)
-                        {
-                            var referenceFrom = classDeclaration.Identifier.Text;
-                            digraph.addEdge(referenceFrom, className);
-                        }
-                    }
-                }
-            }
-        }
-
-        static void pushVerticesClassesToDigraph(Digraph digraph, IEnumerable<SyntaxTree> trees)
-        {
-            foreach (var tree in trees)
-            {
-                var classes = tree.GetRoot().DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
-                if (classes != null)
-                {
-                    foreach (var c in classes)
-                    {
-                        digraph.addVertex(c.Identifier.Text);
-                    }
-                 }
-            }
-
-          //digraph.show();
         }
     }
 }
